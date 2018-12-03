a2b5730656651bb8e4440f43f48ec3bbce870453
Vladyslav <45554407+web-bond@users.noreply.github.com>
2018-12-03 15:42:58 +0200

    Update example.php

--- src/example.php
+++ src/example.php
@@ -4,7 +4,8 @@ define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'pdo_test');
-require_once(dirname(__FILE__).'/src/pdo.php');
+//require_once(dirname(__FILE__).'/src/pdo.php');
+require_once('pdo.php');
 $db = new mPDO(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 $query = $db->query("SELECT * FROM `customer` WHERE `name` LIKE 'John'");
